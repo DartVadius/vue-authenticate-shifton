282f2ebe241a7d6f9f8c9e458ac4682f7c6652eb
dartvadius <vad261275@gmail.com>
2024-06-10 17:39:34 +0300

    migrate to vue3

--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
 import './utils.js';
 import VueAuthenticate from './authenticate.js';
 
-/**
- * VueAuthenticate plugin
- * @param {Object} Vue
- * @param {Object} options
- */
-function plugin(Vue, options) {
-  if (plugin.installed) {
-    return;
-  }
+// /**
+//  * VueAuthenticate plugin
+//  * @param {Object} Vue
+//  * @param {Object} options
+//  */
+// function plugin(Vue, options) {
+//   if (plugin.installed) {
+//     return;
+//   }
+//
+//   plugin.installed = true;
+//
+//   let vueAuthInstance = null;
+//   Object.defineProperties(Vue.prototype, {
+//     $auth: {
+//       get() {
+//         if (!vueAuthInstance) {
+//           // Request handler library not found, throw error
+//           if (!this.$http) {
+//             throw new Error('Request handler instance not found');
+//           }
+//
+//           vueAuthInstance = new VueAuthenticate(this.$http, options);
+//         }
+//         return vueAuthInstance;
+//       },
+//     },
+//   });
+// }
+//
+// /**
+//  * External factory helper for ES5 and CommonJS
+//  * @param  {Object} $http     Instance of request handling library
+//  * @param  {Object} options   Configuration object
+//  * @return {VueAuthenticate}  VueAuthenticate instance
+//  */
+// plugin.factory = function ($http, options) {
+//   return new VueAuthenticate($http, options);
+// };
+//
+// export default plugin;
+export default {
+  install: (app, options) => {
 
-  plugin.installed = true;
+    let vueAuthInstance = null;
+    app.config.globalProperties.$auth = new VueAuthenticate(app.$http, options)
 
-  let vueAuthInstance = null;
-  Object.defineProperties(Vue.prototype, {
-    $auth: {
+    app.config.globalProperties.$auth.get = {
       get() {
         if (!vueAuthInstance) {
           // Request handler library not found, throw error
@@ -27,18 +60,7 @@ function plugin(Vue, options) {
         }
         return vueAuthInstance;
       },
-    },
-  });
+
+    }
+  }
 }
-
-/**
- * External factory helper for ES5 and CommonJS
- * @param  {Object} $http     Instance of request handling library
- * @param  {Object} options   Configuration object
- * @return {VueAuthenticate}  VueAuthenticate instance
- */
-plugin.factory = function ($http, options) {
-  return new VueAuthenticate($http, options);
-};
-
-export default plugin;
